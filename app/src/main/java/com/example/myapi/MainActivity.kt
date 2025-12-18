7cfbf95499f0d6a37ca10f6570e014a7be7b6a91
dzakyputra45 <dzakypratama45@gmail.com>
2025-12-18 19:31:01 +0700

    Menambahkan MainActivity sebagai entry point aplikasi

---
+++ app/src/main/java/com/example/myapi/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.myapi.ui.theme.MyAPITheme
+import com.example.myapi.uicontroller.DataSiswaApp
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContent {
+            MyAPITheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    DataSiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
