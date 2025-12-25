7a3de325261144e240ebe94157d5d609383c5de5
dzakyputra45 <dzakypratama45@gmail.com>
2025-12-25 19:37:12 +0700

    Tambahkan DestinasiEdit agar ke direct ke halaman edit

---
+++ app/src/main/java/com/example/myapi/uicontroller/route/DestinasiEdit.kt
@@ -0,0 +1,11 @@
+package com.example.myapi.uicontroller.route
+
+import com.example.myapi.R
+
+object DestinasiEdit {
+    const val route = "edit_siswa"
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+
+    val titleRes = R.string.edit_siswa
+}
