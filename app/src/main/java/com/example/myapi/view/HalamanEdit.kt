759d88a61d28bdd9d294caba426fe3bce2f53ef3
dzakyputra45 <dzakypratama45@gmail.com>
2025-12-25 19:38:58 +0700

    Tambahkan HalamanEdit berfungsi untuk edit data siswa

---
+++ app/src/main/java/com/example/myapi/view/HalamanEdit.kt
@@ -0,0 +1,48 @@
+package com.example.myapi.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myapi.uicontroller.route.DestinasiEdit
+import com.example.myapi.viewmodel.EditViewModel
+import com.example.myapi.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.layout.padding
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
